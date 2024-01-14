6afc63949dc00c5aac7940a008fd1e9e514c9095
AlexHereticVlasov <62911435+AlexHereticVlasov@users.noreply.github.com>
2024-01-14 23:53:18 +0300

    Health UI Update

--- Assets/Scripts/UI/PlayerHealthView.cs
+++ Assets/Scripts/UI/PlayerHealthView.cs
@@ -1,14 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public sealed class PlayerHealthView : MonoBehaviour
 {
+    private const float LerpFactor = 0.03125f;
+
     [SerializeField] private PlayerUIPresenter _healthPresenter;
     [SerializeField] private Slider _healthBar;
+    [SerializeField] private Slider _aditionalBar;
+
+    private Coroutine _change;
 
     private void OnEnable() => _healthPresenter.ValueChanged += OnHealthChanged;
 
     private void OnDisable() => _healthPresenter.ValueChanged -= OnHealthChanged;
 
-    private void OnHealthChanged(float value) => _healthBar.value = value;
+    private void OnHealthChanged(float value)
+    {
+        if (_change is not null)
+            StopCoroutine(_change);
+
+        _aditionalBar.value = value;
+        _change = StartCoroutine(ChangeValue(value));
+    }
+
+    private IEnumerator ChangeValue(float target)
+    {
+        while (_healthBar.value != target)
+        {
+            _healthBar.value = Mathf.Lerp(_healthBar.value, target, LerpFactor);
+            yield return null;
+        }
+
+        _change = null;
+    }
 }
